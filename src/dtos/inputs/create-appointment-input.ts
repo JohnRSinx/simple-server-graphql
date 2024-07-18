543551b5d807703c81146ddacbfe840c80561436
JohnRSinx <johnrodrigues97106@gmail.com>
2024-07-18 11:30:59 -0300

    created schema

---
+++ src/dtos/inputs/create-appointment-input.ts
@@ -0,0 +1,12 @@
+import { Field, InputType } from "type-graphql";
+
+@InputType()
+export class CreateAppointmentInput {
+  @Field()
+  customerId: string;
+  @Field()
+  startsAt: Date;
+
+  @Field()
+  endsAt: Date;
+}
